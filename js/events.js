2ba764d0253b119efdfe334c11c4c77a06968e17
Cory Pavitt <corypavitt@hotmail.com>
2018-05-15 16:50:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,9 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('form').on('submit',)
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
 }
 //define functions here
 
